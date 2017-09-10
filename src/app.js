6d19ecea388bc16510d1673aebd0edd263db3f52
Terry Seidler <terry.seidler@ogd.nl>
2017-09-10 21:23:06 +0200

    Set up webpack with less-loader

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+import Style from "../styles/main.less";
+
 const PIXI = require("pixi.js");
 const inherits = require("inherits");
 
 function BomberThing() {
     let t = new PIXI.Text("Hello PIXI.js!");
-    this.addChild(t);
 }
 
 new BomberThing();
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,49 @@
-var path = require("path");
-var webpack = require("webpack");
+const path = require("path");
+const webpack = require("webpack");
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractLess = new ExtractTextPlugin("style.css");
 
 module.exports  = {
     entry: path.resolve(__dirname, "src/app.js"),
     output: {        
-        path: path.resolve(__dirname, "/build"),
+        path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
     },
     module: {
-        loaders: [
-            {
+        rules: [{
                 test: /\.js$/,
-                loader: "babel-loader",
-                query: {
-                    presets: [
-                        ["env", {
-                            "targets": {
-                              "browsers": ["last 2 versions"]
-                            },
-                            "debug": true
-                        }]
-                    ]
-                }
-            }
-        ]
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                ["env", {
+                                    "targets": {
+                                    "browsers": ["last 2 versions"]
+                                    },
+                                    "debug": true
+                                }]
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test:  /\.less$/i,
+                use: extractLess.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "less-loader"
+                    }],
+                    fallback: "style-loader"
+                })
+        }]
     },
+    plugins: [
+        extractLess
+    ],
     node: {
         fs: "empty"
     }
