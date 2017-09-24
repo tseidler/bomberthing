0767fa364c86a74b887ad9238d70c840efa5c6bd
Terry Seidler <terry.seidler@ogd.nl>
2017-09-24 22:13:23 +0200

    Set random background color every few frames

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const GAME_HEIGHT = 1024;
 const GAME_WIDTH = 768;
 
 const renderer = PIXI.autoDetectRenderer(GAME_HEIGHT, GAME_WIDTH);
+const stage = new PIXI.Container();
 document.getElementById("gameContainer").appendChild(renderer.view);
 
-const game = new Game(renderer, GAME_HEIGHT, GAME_HEIGHT);
+const game = new Game(renderer, stage, GAME_HEIGHT, GAME_HEIGHT);
+game.start();
--- src/game.js
+++ src/game.js
@@ -1,9 +1,41 @@
+const COLOR_CHANGE_TIME = 150;
+
 class Game {
-    constructor(renderer, height, width) {
+    constructor(renderer, stage, height, width) {
         this.renderer = renderer;
         this.height = height;
         this.width = width;
+        this.stage = stage;
+
+        this.time_since_color_change = 0;
     }
+
+    start() {
+        this.lastUpdate = Date.now();
+        this.gameLoop();
+    }
+
+    gameLoop() {
+        let elapsed = Date.now() - this.lastUpdate;
+        this.time_since_color_change += elapsed;
+
+        if(this.time_since_color_change >= COLOR_CHANGE_TIME) {
+            this.time_since_color_change = 0;
+            this.renderer.backgroundColor = get_random_color();
+        }
+
+        this.renderer.render(this.stage);
+
+        this.lastUpdate = Date.now();
+        requestAnimationFrame( () => { this.gameLoop() });
+    }
+}
+function get_random_color() {
+    let c = () => {
+        var hex = Math.floor(Math.random()*256).toString(16);
+        return ("0"+String(hex)).substr(-2); // pad with zero
+    }
+    return parseInt(c()+c()+c(), 16);
 }
 
 export default Game;
