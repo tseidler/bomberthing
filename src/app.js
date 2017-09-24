28c6ccbb2cbbc7f3d0501f4719cee0a1d7fb8db0
Terry Seidler <terry.seidler@ogd.nl>
2017-09-24 21:30:05 +0200

    Add Game class

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import Style from "../styles/main.less";
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
+import Game from "../src/game.js";
 
-const inherits = require("inherits");
+const GAME_HEIGHT = 1024;
+const GAME_WIDTH = 768;
 
-function BomberThing() {
-    let t = new PIXI.Text("Hello PIXI.js!");
-}
+const renderer = PIXI.autoDetectRenderer(GAME_HEIGHT, GAME_WIDTH);
+document.getElementById("gameContainer").appendChild(renderer.view);
 
-new BomberThing();
+const game = new Game(renderer, GAME_HEIGHT, GAME_HEIGHT);
---
+++ src/game.js
@@ -0,0 +1,9 @@
+class Game {
+    constructor(renderer, height, width) {
+        this.renderer = renderer;
+        this.height = height;
+        this.width = width;
+    }
+}
+
+export default Game;
