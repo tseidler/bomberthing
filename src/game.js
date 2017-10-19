73c8c69fdc819062e2d87da92169a3cc0934a32a
Terry Seidler <terry.seidler@ogd.nl>
2017-10-19 20:03:15 +0200

    Draw the level

--- src/game.js
+++ src/game.js
@@ -3,42 +3,51 @@ import Level from './level';
 import * as PIXI from "pixi.js";
 
 class Game {
-    constructor(container, height, width) {
+    constructor(container, width, height) {
         this.container = container;
 
         this.height = height;
         this.width = width;
-        this.renderer = PIXI.autoDetectRenderer(height, width);
-        this.stage = new PIXI.Container();
+        this.renderer = PIXI.autoDetectRenderer(width, height);
+        this.gameStage = new PIXI.Container();
+        this.levelStage = new PIXI.Container();
+        
         this.container.appendChild(this.renderer.view);
 
         this.time_since_color_change = 0;
     }
 
-    start() {
-        this.lastUpdate = Date.now();
-        this.level = new Level(30, 20, consts.LEVELS[Math.floor(Math.random() * consts.LEVELS.length)]);  // select a random level
-
+    load() {
+        // select a random level for loading        
+        this.level = new Level(30, 20, consts.LEVELS[Math.floor(Math.random() * consts.LEVELS.length)]);
         this.loadResources(consts.TEXTURE_MAP);
     }
-
+    
     loadResources(resources) {
         for(const key in resources) {
             PIXI.loader.add(resources[key]);
         }
-        PIXI.loader.load(this.gameLoop.bind(this));
+        PIXI.loader.load(this.start.bind(this));
+    }
+
+    start() {
+        // get the level stage once (will never change)
+        this.levelStage = this.level.getStage();
+        this.lastUpdate = Date.now();
+        this.gameLoop();
     }
 
     gameLoop() {
         let elapsed = Date.now() - this.lastUpdate;
-        this.time_since_color_change += elapsed;
+        // this.time_since_color_change += elapsed;
 
-        if(this.time_since_color_change >= consts.COLOR_CHANGE_TIME) {
-            this.time_since_color_change = 0;
-            this.renderer.backgroundColor = get_random_color();
-        }
+        // if(this.time_since_color_change >= consts.COLOR_CHANGE_TIME) {
+        //     this.time_since_color_change = 0;
+        //     this.renderer.backgroundColor = get_random_color();
+        // }
 
-        this.renderer.render(this.stage);
+        this.renderer.render(this.levelStage);
+        // this.renderer.render(this.gameStage);
 
         this.lastUpdate = Date.now();
         requestAnimationFrame( () => { this.gameLoop() });
--- src/level.js
+++ src/level.js
@@ -1,7 +1,34 @@
+import * as PIXI from "pixi.js";
+import {TEXTURE_MAP} from "./consts";
+
+const TILE_WIDTH = 64;
+const TILE_HEIGHT = 64;
 export default class Level {
-    constructor(width = 30, height = 20, layout) {
-        this.width = width;
-        this.height = height;
+    constructor(width = 17, height = 12, layout) {
+        this.num_horizontal_tiles = width;
+        this.num_vertical_tiles = height;
         this.layout = layout;
+    }
+
+    getStage() {
+        let stage = new PIXI.Container();
+        let y = 0;
+        for(let row of this.layout) {
+            let x = 0;
+            for(let tile of row) {
+                let texture_filename = TEXTURE_MAP[tile];
+                let sprite = new PIXI.Sprite(PIXI.loader.resources[texture_filename].texture);
+                sprite.x = x;
+                sprite.y = y;
+                sprite.height = TILE_HEIGHT;
+                sprite.width = TILE_WIDTH;
+                // debugger
+                stage.addChild(sprite);
+
+                x += TILE_WIDTH;
+            }
+            y += TILE_HEIGHT;
+        }
+        return stage;
     }
 }
