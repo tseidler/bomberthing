7386d27d17fa8b6ff672beed4786a1742abf60f7
Terry Seidler <terry.seidler@ogd.nl>
2017-11-12 16:56:14 +0100

    Make player move on key presses

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 import * as consts from './consts';
+import GameControls from './gamecontrols';
 import Level from './level';
 import Player from './player';
 import * as PIXI from "pixi.js";
@@ -36,6 +37,8 @@ class Game {
         this.player = new Player("Player1", consts.TILE_HEIGHT, consts.TILE_WIDTH, consts.TEXTURE_MAP['cat']);
         this.stage.addChild(this.player.sprite);
 
+        this.setupControls();
+
         this.lastUpdate = Date.now();
         this.gameLoop();
     }
@@ -43,10 +46,24 @@ class Game {
     gameLoop() {
         let elapsed = Date.now() - this.lastUpdate;
 
+        this.player.update(elapsed);
         this.renderer.render(this.stage);
 
         this.lastUpdate = Date.now();
         requestAnimationFrame( () => { this.gameLoop() });
+    }
+
+    setupControls() {
+        this.controls = new GameControls();
+
+        // up (38)
+        this.controls.setKey(38, () => { this.player.setMovement("up") }, () => { this.player.clearMovement("up") });
+        // down (40)
+        this.controls.setKey(40, () => { this.player.setMovement("down") }, () => { this.player.clearMovement("down") });
+        // left (37)
+        this.controls.setKey(37, () => { this.player.setMovement("left") }, () => { this.player.clearMovement("left") });
+        // right (39)
+        this.controls.setKey(39, () => { this.player.setMovement("right" )}, () => { this.player.clearMovement("right") });
     }
 }
 
--- src/player.js
+++ src/player.js
@@ -10,5 +10,46 @@ export default class Player {
         this.sprite.y = y;
         this.sprite.width = TILE_WIDTH;
         this.sprite.height = TILE_HEIGHT;
+
+        this.movement = {x: 0, y: 0};
+    }
+
+    update(elapsedTime) {
+        this.sprite.x += this.movement.x * elapsedTime;
+        this.sprite.y += this.movement.y * elapsedTime;
+    }
+
+    setMovement(direction) {
+        switch(direction) {
+            case "up":
+                this.movement.y = -PLAYER_MOVE_SPEED;
+                break;
+            case "down":
+                this.movement.y = PLAYER_MOVE_SPEED;
+                break;
+            case "left":
+                this.movement.x = -PLAYER_MOVE_SPEED;
+                break;
+            case "right":
+                this.movement.x = PLAYER_MOVE_SPEED;
+                break;
+        }
+    }
+
+    clearMovement(direction) {
+        switch(direction) {
+            case "up":
+                this.movement.y = Math.max(this.movement.y, 0);
+                break;
+            case "down":
+                this.movement.y = Math.min(this.movement.y, 0);
+                break;
+            case "left":
+                this.movement.x = Math.max(this.movement.x, 0);
+                break;
+            case "right":
+                this.movement.x = Math.min(this.movement.x, 0);
+                break;
+        }
     }
 }
